bc8e5df1f70a60e5d53f657c43778fb246c13764
adanminhas <adanminhas@hotmail.com>
2026-05-24 16:07:20 +0100

    feat(frontend): add streams.health and playlistUrl helper

--- frontend/src/api/streams.js
+++ frontend/src/api/streams.js
@@ -1,12 +1,18 @@
-import axios from "axios";
+import client, { API_BASE_URL } from "./client";
 
-const API = "http://localhost:8000/api/streams";
+const PATH = "/api/streams";
 
 export default {
   start(id) {
-    return axios.post(`${API}/${id}/start`);
+    return client.post(`${PATH}/${id}/start`);
   },
   stop(id) {
-    return axios.post(`${API}/${id}/stop`);
+    return client.post(`${PATH}/${id}/stop`);
+  },
+  health(id) {
+    return client.get(`${PATH}/${id}/health`);
+  },
+  playlistUrl(id) {
+    return `${API_BASE_URL}/streams/${id}/index.m3u8`;
   },
 };

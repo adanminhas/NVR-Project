748880ee9397111222c9f2eec4388eb136029572
adanminhas <adanminhas@hotmail.com>
2026-05-26 12:02:36 +0100

    feat(backend): add User model

---
+++ backend/app/models/user_model.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, Integer, String
+
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(100), nullable=False, unique=True, index=True)
+    password_hash = Column(String(255), nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)

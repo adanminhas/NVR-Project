8138feaaf8f4d1926acf1d15d36ad773ad60cc4c
adanminhas <adanminhas@hotmail.com>
2026-05-27 15:57:23 +0100

    chore(frontend): inline vitest config in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  test: {
+    environment: "jsdom",
+    globals: true,
+    environmentOptions: {
+      jsdom: {
+        url: "http://localhost",
+      },
+    },
+  },
+});
